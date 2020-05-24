f8dd541b7802efb50c035e631a070c9a6d3388f0
Dominik Zöchbauer <29693067+dzoech@users.noreply.github.com>
2020-05-25 00:10:14 +0200

    added first draft for command line parsing using subcommands

---
+++ src/Nudelsieb/Nudelsieb.Cli/AddCommand.cs
@@ -0,0 +1,25 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Nudelsieb.Cli
+{
+    class AddCommand : CommandBase
+    {
+        [Argument(0)]
+        [Required]
+        public string Message { get; set; }
+
+        [Option]
+        public string[] Groups { get; set; }
+
+        protected override int OnExecute(CommandLineApplication app)
+        {
+            Console.WriteLine($"Adding {Message} with {Groups.Length} groups: '{string.Join(", ", Groups)}'");
+
+            return base.OnExecute(app);
+        }
+    }
+}
--- src/Nudelsieb/Nudelsieb.Cli/Program.cs
+++ src/Nudelsieb/Nudelsieb.Cli/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace Nudelsieb.Cli
 {
-    class Program
+    // See example: https://github.com/natemcmaster/CommandLineUtils/blob/master/docs/samples/subcommands/inheritance/Program.cs
+    [HelpOption("-?|-h|--help")]
+    abstract class CommandBase
     {
-        static void Main(string[] args)
+        protected virtual int OnExecute(CommandLineApplication app)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Result = nudelsieb " + ArgumentEscaper.EscapeAndConcatenate(args));
+            app.ShowHelp();
+            return 0;
         }
+    }
+
+    [Command("nudelsieb")]
+    [VersionOptionFromMember("--version", MemberName = nameof(GetVersion))]
+    [Subcommand(
+        typeof(AddCommand))]
+    class Program : CommandBase
+    {
+        public static void Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        protected override int OnExecute(CommandLineApplication app)
+        {
+            // this shows help even if the --help option isn't specified
+            app.ShowHelp();
+            return 1;
+        }
+
+        private static string GetVersion()
+            => typeof(Program).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
     }
 }
