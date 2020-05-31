0e347e4848be5f4e3136d0ab7e4aae83548b7c2d
Dominik Zöchbauer <29693067+dzoech@users.noreply.github.com>
2020-05-31 15:27:40 +0200

    enabled nullable reference type

--- src/Nudelsieb/Nudelsieb.WebApi/WeatherForecast.cs
+++ src/Nudelsieb/Nudelsieb.WebApi/WeatherForecast.cs
@@ -10,6 +10,6 @@ namespace Nudelsieb.WebApi
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
     }
 }
