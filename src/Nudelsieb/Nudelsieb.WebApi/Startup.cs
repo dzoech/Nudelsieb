07b7ff9a6b3f0b7e947f34bf817b446651a17a89
Dominik Zöchbauer <29693067+dzoech@users.noreply.github.com>
2020-05-31 16:38:08 +0200

    added swagger generator incl ui

--- src/Nudelsieb/Nudelsieb.WebApi/Startup.cs
+++ src/Nudelsieb/Nudelsieb.WebApi/Startup.cs
@@ -12,11 +12,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Nudelsieb.WebApi
 {
     public class Startup
     {
+        const string ApiName = "Braindump";
+        const string ApiVersion = "V1";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -29,7 +33,13 @@ namespace Nudelsieb.WebApi
         {
             services.AddAuthentication(AzureADB2CDefaults.BearerAuthenticationScheme)
                 .AddAzureADB2CBearer(options => Configuration.Bind("AzureAdB2C", options));
+
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = ApiName, Version = ApiVersion });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +60,13 @@ namespace Nudelsieb.WebApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{ApiName} {ApiVersion}");
             });
         }
     }
