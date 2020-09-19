1ac87583213c07f04f6066e10b53da8d38929a39
Dominik Zöchbauer <29693067+dzoech@users.noreply.github.com>
2020-09-20 00:10:46 +0200

    replaced path './' with $PSScriptRoot

--- build/cli/build-self-contained.ps1
+++ build/cli/build-self-contained.ps1
@@ -12,12 +12,12 @@ Param(
 
 Write-Host "Building version $Version"
 
-rmdir ./chocolatey/tools/win-x64/* -Force -Recurse -ErrorAction SilentlyContinue
-dotnet publish --configuration Release --runtime win-x64 --output ./chocolatey/tools/win-x64 --self-contained true -p:AssemblyVersion=$Version -p:PublishReadyToRun=true -p:PublishTrimmed=true -p:DebugType=None --verbosity minimal ./../../src/Nudelsieb/Nudelsieb.Cli/Nudelsieb.Cli.csproj
+rmdir $PSScriptRoot/chocolatey/tools/win-x64/* -Force -Recurse -ErrorAction SilentlyContinue
+dotnet publish --configuration Release --runtime win-x64 --output $PSScriptRoot/chocolatey/tools/win-x64 --self-contained true -p:AssemblyVersion=$Version -p:PublishReadyToRun=true -p:PublishTrimmed=true -p:DebugType=None --verbosity minimal $PSScriptRoot/../../src/Nudelsieb/Nudelsieb.Cli/Nudelsieb.Cli.csproj
 
-choco pack --version $Version --outputdirectory $OutputPath ./chocolatey/nudelsieb-cli.nuspec
+choco pack --version $Version --outputdirectory $OutputPath $PSScriptRoot/chocolatey/nudelsieb-cli.nuspec
 
-choco uninstall nudelsieb-cli -ErrorAction SilentlyContinue
+choco uninstall nudelsieb-cli | Out-Null
 
 mkdir -f $OutputPath # -f to suppress error when already existing
 
@@ -32,5 +32,5 @@ nudelsieb --version
 
 if ($Publish) {
     # only for publishing from local dev environment
-    choco push "./$OutputPath/nudelsieb-cli.$($Version).nupkg" --source https://push.chocolatey.org
+    choco push "$OutputPath/nudelsieb-cli.$($Version).nupkg" --source https://push.chocolatey.org
 }
