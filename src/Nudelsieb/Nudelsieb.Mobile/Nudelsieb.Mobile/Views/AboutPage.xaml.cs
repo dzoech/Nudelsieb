9f2b7a09bac8e9d8998c4caf9361de833f8cdbd4
Dominik Zöchbauer <29693067+dzoech@users.noreply.github.com>
2020-12-31 00:09:11 +0100

    Add scaffolded Xamarin projects (Forms and Android) and install NuGet packages for Azure Notification Hub

---
+++ src/Nudelsieb/Nudelsieb.Mobile/Nudelsieb.Mobile/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Nudelsieb.Mobile.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Nudelsieb/Nudelsieb.Mobile/Nudelsieb.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Nudelsieb.Mobile.ViewModels;
+using Xamarin.Forms;
+
+namespace Nudelsieb.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
