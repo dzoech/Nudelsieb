77a261df7e2bdec3a928f7fb273df858e1d9820f
Dominik Zöchbauer <29693067+dzoech@users.noreply.github.com>
2021-03-19 11:36:19 +0100

    Remove orphaned code and configuration regarding CosmosDb

--- src/Nudelsieb/Nudelsieb.WebApi/Startup.cs
+++ src/Nudelsieb/Nudelsieb.WebApi/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Nudelsieb.Domain;
-using Microsoft.Azure.Cosmos;
 using Microsoft.IdentityModel.Logging;
 using Nudelsieb.Persistence.Relational;
 using Microsoft.EntityFrameworkCore;
@@ -37,19 +36,10 @@ namespace Nudelsieb.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // TODO implement cosmos db persistence 
-            //var cosmosDbContainer = InitializeCosmosDbContainerAsync(
-            //    Configuration.GetSection("Persistence").GetSection("CosmosDb"))
-            //    .Result;
-
-            // add as singleton to enable in-memory data for dummy repository
-            services.AddTransient<INeuronRepository, RelationalDbNeuronRepository>();
-            //services.AddSingleton<Container>(_ => cosmosDbContainer);
-
-            services.AddApplicationLayer();
-            services.AddNotificationServices(options => Configuration.Bind(NotificationsOptions.SectionName, options));
-
             services
+                .AddTransient<INeuronRepository, RelationalDbNeuronRepository>()
+                .AddApplicationLayer()
+                .AddNotificationServices(options => Configuration.Bind(NotificationsOptions.SectionName, options))
                 .AddAuthentication(AzureADB2CDefaults.BearerAuthenticationScheme)
                 .AddAzureADB2CBearer(options => Configuration.Bind("AzureAdB2C", options));
 
@@ -132,25 +122,6 @@ namespace Nudelsieb.WebApi
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{ApiName} {ApiVersion}");
             });
-        }
-
-        private async Task<Container> InitializeCosmosDbContainerAsync(IConfigurationSection configSection)
-        {
-            // var b = new CosmosClientBuilder("",""). ...
-
-            var accountConfig = configSection.GetSection("Account");
-            CosmosClient client = new CosmosClient(
-                accountConfig.GetValue<string>("Endpoint"),
-                accountConfig.GetValue<string>("Key"));
-
-            Database database = await client.CreateDatabaseIfNotExistsAsync(
-                configSection.GetValue<string>("Database"));
-
-            Container container = await database.CreateContainerIfNotExistsAsync(
-                configSection.GetValue<string>("Container"),
-                configSection.GetValue<string>("PartitionKeyPath"));
-
-            return container;
         }
     }
 }
