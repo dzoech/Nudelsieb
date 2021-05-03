da7d4ecbc7b776647d990da5d14d091f17582fc3
Dominik Zöchbauer <29693067+dzoech@users.noreply.github.com>
2021-05-03 21:35:55 +0200

    Scaffold Angular applications using Nx.js

---
+++ src/Nudelsieb/Nudelsieb.Angular/apps/nudelsieb-ng/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nudelsieb-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'nudelsieb-ng';
+}
