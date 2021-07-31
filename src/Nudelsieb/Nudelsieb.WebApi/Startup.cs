e077d44181535c15cbc94389722fb20d3a34757a
Dominik Zöchbauer <29693067+dzoech@users.noreply.github.com>
2021-07-31 21:44:50 +0200

    Add Application Insights dependencies to WebApi project

--- src/Nudelsieb/Nudelsieb.WebApi/Startup.cs
+++ src/Nudelsieb/Nudelsieb.WebApi/Startup.cs
@@ -87,6 +87,8 @@ namespace Nudelsieb.WebApi
                 options.UseSqlServer(connStr);
                 options.EnableSensitiveDataLogging();
             });
+
+            services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request
